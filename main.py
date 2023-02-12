aaa1337ec1c9d26907aae8a26a3496abf18ac271
clairehzhao <claire.zhao1234@gmail.com>
2023-02-11 22:23:44 -0800

    symptom add init

--- main.py
+++ main.py
@@ -55,6 +55,7 @@ def activate_job():
     initPeriods()
     initComments()
     initScores()
+    initSymptoms()
 
 # this runs the application on the development server
 if __name__ == "__main__":
