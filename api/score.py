4b0d766979973fc1dae7fe8a6a2330d50438ef54
gwang1224 <gracewang187@gmail.com>
2023-02-22 23:24:39 -0800

    del method doesn't work :(

--- api/score.py
+++ api/score.py
@@ -47,10 +47,17 @@ class ScoreAPI:
             json_ready = [user.read() for user in users]  # prepare output in json
             return jsonify(json_ready)  # jsonify creates Flask response object, more specific to APIs than json.dumps
     
-
+    class _Delete(Resource):
+        def delete(self):
+            body = request.get_json()
+            uid = body.get('uid')
+            score = Score.query.get(uid)
+            score.delete()
+            return f"{score.read()} Has been deleted"
             
 
     # building RESTapi endpoint
     api.add_resource(_Create, '/create')
     api.add_resource(_Read, '/')
+    api.add_resource(_Delete, '/delete')
     
