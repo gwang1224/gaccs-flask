9d36382342d3f0bb0bfd528347a1872614a19597
clairehzhao <claire.zhao1234@gmail.com>
2023-02-24 18:34:39 -0800

    ovulation db

--- api/ovulation.py
+++ api/ovulation.py
@@ -18,13 +18,15 @@ class OvulationAPI:
             body = request.get_json()
             
             ''' Avoid garbage in, error checking '''
+            nextovulation = body.get('nextovulation')
             perioddate = body.get('perioddate')
             periodcycle = body.get('periodcycle')
             menscycle = body.get('menscycle')
             ''' #1: Key code block, setup USER OBJECT '''
-            uo = Ovulation(perioddate=perioddate,
-                        periodcycle=periodcycle,
-                        menscycle=menscycle)
+            uo = Ovulation(nextovulation=nextovulation,
+                            perioddate=perioddate,
+                            periodcycle=periodcycle,
+                            menscycle=menscycle)
             
             ''' #2: Key Code block to add user to database '''
             # create user in database
